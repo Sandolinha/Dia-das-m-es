2ca255e79d10a4102cb3c0c036b7e9f0f4d4ad9a
Fernando Arthur <fernandoarthurgomes624@gmail.com>
2025-05-05 13:07:20 -0300

    Add files via upload

---
+++ diam.py
@@ -0,0 +1,27 @@
+import time
+
+mensagem = [
+    "Feliz dias das mães!", 
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️", 
+    "Mãe, obrigado por tudo que você fez por mim.", 
+    "Sempre te amarei, você é especial do jeitinho do que você é.", 
+    "Te amo.", 
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️" 
+]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5)
+
+print()
+print("   *****     *****")
+print(" *******   *******")
+print("********* *********")
+print("*******************")
+print(" *****************")
+print("   ************")
+print("    **********")
+print("     ********")
+print("      ******")
+print("       ****")
+print("        **")
